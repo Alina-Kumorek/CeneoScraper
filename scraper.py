3b3b59a967ec3bcabe335ddff81ed8a15705e995
Alina-Kumorek <228103@student.uek.krakow.pl>
2023-03-25 15:21:59 +0100

    2023-03-25 AK

---
+++ scraper.py
@@ -0,0 +1,11 @@
+import requests
+
+# product_code = input("Podaj kod produktu: ")
+product_code = "95733300"
+
+# url = "https://www.ceneo.pl/" + product_code + "#tab=reviews"
+url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+# url = "https://www.ceneo.pl/{}#tab=reviews".format(product_code)
+
+response = requests.get(url)
+print(response.status_code)
