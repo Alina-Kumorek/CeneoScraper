aa61cea1e8237ffbb81cd17f387f5f79a6032a04
Alina Kumorek <228103@student.uek.krakow.pl>
2023-05-30 21:49:53 +0200

    2023-05-30 AK

--- analyzer.py
+++ analyzer.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # print(*[filename.removesuffix(".json") for filename in os.listdir("./opinions")], sep="\n")
-# product_code = input("Podaj kod produktu: ")
+# product_code = float(input("Podaj kod produktu: "))
 product_code = 39562616
 
 opinions = pd.read_json(f"opinions/{product_code}.json")
@@ -22,6 +22,15 @@ Dla {stats["pros_count"]} opinii podana została lista zalet produktu,
 a dla {stats["cons_count"]} opinii podana została lista jego wad.
 Średnia ocena produktu wynosii {stats["average_score"]:.2f}.""")
 
+if not os.path.exists("./plots"):
+    os.mkdir("./plots")
+
 stars = opinions.score.value_counts().reindex(list(np.arange(0, 5.5, 0.5)), fill_value=0)
 stars.plot.bar()
+plt.savefig(f"plots/{product_code}_stars.jpg")
+plt.show()
+
+recs = opinions.recommendation.fillna("Brak").value_counts().reindex(["Polecam", "Nie polecam", "Brak"], fill_value=0)
+recs.plot.pie()
+plt.savefig(f"plots/{product_code}_recs.jpg")
 plt.show()
--- scraper.py
+++ scraper.py
@@ -26,8 +26,8 @@ selectors = {
     "likes": ("button.vote-yes", "data-total-vote"),
     "dislikes": ("button.vote-no", "data-total-vote"),
     "content": ("div.user-post__text",),
-    "cons": ("div.review-feature__title--positives ~ div.review-feature__item", None, True),
-    "pros": ("div.review-feature__title--negatives ~ div.review-feature__item", None, True),
+    "cons": ("div.review-feature__title--negatives ~ div.review-feature__item", None, True),
+    "pros": ("div.review-feature__title--positives ~ div.review-feature__item", None, True),
 }
 
 product_code = input("Podaj kod produktu: ")
